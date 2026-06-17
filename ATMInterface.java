ca808c9c575cd1c6e6c82bd4e1cb10de6caa0962
rayapureddypravallika23-glitch <rayapureddypravallika23@gmail.com>
2026-06-17 16:23:25 +0530

    Add files via upload

---
+++ ATMInterface.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class BankAccount {
+    double balance = 10000;
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    void checkBalance() {
+        System.out.println("Balance: " + balance);
+    }
+}
+
+public class ATMInterface {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BankAccount account = new BankAccount();
+
+        while (true) {
+            System.out.println("\n1.Deposit");
+            System.out.println("2.Withdraw");
+            System.out.println("3.Check Balance");
+            System.out.println("4.Exit");
+
+            System.out.print("Choose option: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter amount: ");
+                    account.deposit(sc.nextDouble());
+                    break;
+                case 2:
+                    System.out.print("Enter amount: ");
+                    account.withdraw(sc.nextDouble());
+                    break;
+                case 3:
+                    account.checkBalance();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+    }
+}
